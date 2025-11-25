0db428880a5faae214959e15e450f90dd5e1fb50
HOST <relkarne@astus.fr>
2025-11-25 10:09:03 +0100

    code last merge

--- accesclient/accesclient/settings.py
+++ accesclient/accesclient/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s=648nrw$m8&fei#++s*ac=sg18d)^-d2q8%q0k^=se9r2uwz='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.214','127.0.0.1','91.151.123.222','webclient.astus.fr']
+ALLOWED_HOSTS = ['192.168.0.214','127.0.0.1','91.151.123.222','192.168.88.3','webclient.astus.fr']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,8 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
+STATIC_ROOT = BASE_DIR /  "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
